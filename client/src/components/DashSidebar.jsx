caf07fd6ed18caaec1fc351c01c0ff060445f4d3
cmlowerence <cmlowerence2777@gmail.com>
2024-04-17 15:12:09 +0530

    add posts section to dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,14 +1,16 @@
 import { Sidebar } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function DashSidebar() {
   const location = useLocation();
   const [tab, setTab] = useState("");
   const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -19,9 +21,9 @@ export default function DashSidebar() {
   }, [location.search]);
 
   const handleLogOut = async () => {
-    try{
-      const res = await fetch('/api/user/signout',{
-        method: 'POST',
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
       });
       const data = res.json();
       if (!res.ok) {
@@ -32,24 +34,39 @@ export default function DashSidebar() {
     } catch (err) {
       console.log(err.message);
     }
-  }
+  };
 
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
-              label={"User"}
+              label={currentUser.isAdmin ? "Admin" : "User"}
               labelColor="dark"
-              as={'div'}
+              as={"div"}
             >
               Profile
             </Sidebar.Item>
           </Link>
-          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer" onClick={handleLogOut}>
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === "posts"}
+                icon={HiDocumentText}
+                as={"div"}
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleLogOut}
+          >
             Log Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
