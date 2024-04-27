fa8215e3898d5d2cffadf2e0f22fc665edaf14f2
cmlowerence <cmlowerence2777@gmail.com>
2024-04-27 16:21:26 +0530

    add show more functionality of the dashboard

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,15 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// import dotenv from 'dotenv';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-// dotenv.config();
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "blog-8dccd.firebaseapp.com",
